6fc6eff914174ecf53308ec2f6ef7fa62a829213
maria carrero <mariacarrerochacon267@gmail.com>
2020-10-26 08:08:40 -0500

    ejercicio de pares o impares para el uso del range

---
+++ range.py
@@ -0,0 +1,13 @@
+# range(comienzo,fin,pasos)
+""" imprime 
+todos los numeros pares del 0 al 100"""
+
+for par in range(0, 101, 2):
+    print(f'{par} ')
+
+
+print("""
+
+          '""")
+for impar in range(1, 100, 2):
+    print(f'{impar} ')
